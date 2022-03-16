c451f9fefbe121c089dcc6b7585dcd41581c54a3
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-16 20:35:14 +0300

    Universal function for animations

---
+++ src/js/helpers/slide.js
@@ -0,0 +1,16 @@
+const animateCSS = (element, animation) =>
+// We create a Promise and return it
+new Promise((resolve, reject) => {
+  element.classList.add('animated', animation);
+
+  // When the animation ends, we clean the classes and resolve the Promise
+  function handleAnimationEnd(event) {
+    event.stopPropagation();
+    element.classList.remove('animated', animation);
+    resolve('Animation ended');
+  }
+
+  element.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
+
+export default animateCSS;
