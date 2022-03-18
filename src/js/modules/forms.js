d46fb14e8f880fbb1893699e3e65489af96fbdb8
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-18 16:30:40 +0300

    mask for usa phone numbers

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -12,6 +12,64 @@ export default class Forms {
   }
 
 
+  phoneMask() {
+    //function to set cursour at right position
+    const setCursourPosition = (position, elem) => {
+      elem.focus(); //forced focus
+
+      if (elem.setSelectionRange) {//if browser has method 'setSelectionRange'
+        elem.setSelectionRange(position, position);
+      }
+      else if (elem.createTextRange) {//for other browsers(expl)
+        let range = elem.createTextRange();
+
+        range.collapse(true);//start position = end one
+        range.moveEnd('character', position);//end position
+        range.moveStart('character', position);//start position(= end position)
+        range.select();//select textrange
+      }
+    };
+    /////////////////////////////////////
+
+    function createMask(event) {
+      let matrix = '+1 (___)___-____'; //Here is our mask
+      let i = 0; //index of symbols
+      let def = matrix.replace(/\D/g, ''), //to show only plus and seven
+          val = this.value.replace(/\D/g, ''); //to show only plus and seven for input
+
+      //to prevent deleting plus and seven during input
+      if (def.length >= val.length) {
+        val = def;
+      }
+
+      this.value = matrix.replace(/./g, a => {
+        //fill the phone mask
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+      });
+
+      //if clicked outside the mask and no sybols in input, then mask become default
+      if (event.type === 'blur') {
+        if (this.value.length === 2) {
+          this.value = '';
+        }
+      }
+      else {
+        //set cursour in the end of val
+        setCursourPosition(this.value.length, this);
+      }
+      /////////////////////////////////////
+    }
+
+    let inputs = document.querySelectorAll('[name="phone"]');
+
+    inputs.forEach(input => {
+      input.addEventListener('input', createMask);
+      input.addEventListener('focus', createMask);
+      input.addEventListener('blur', createMask);
+    });
+  }
+
+
   //post data to the server
   async fetchData(url, data) {
     let result = fetch(url, {
@@ -32,6 +90,8 @@ export default class Forms {
 
 
   init() {
+    this.phoneMask();
+
     this.forms.forEach((form) => {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
